bd720550b308d1ffadde4880300677a93ae901ef
Mr zakir ullah <mrzak802@gmail.com>
2024-05-04 10:24:55 -0700

    comment

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import "./propertyList.css"
+
+function PropertyList() {
+  return (
+    <div className='propertyList'>
+        <div className="propertyListItem">
+            <img src="https://images.pexels.com/photos/164558/pexels-photo-164558.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className='propertyListImg'
+             alt="" />
+             <div className="propertListTitle">
+                <h2>Hotel</h2>
+                <h3>233 hotels</h3>
+
+             </div>
+        </div>
+        <div className="propertyListItem">
+            <img src="https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className='propertyListImg'
+             alt="" />
+             <div className="propertListTitle">
+                <h2>Hotel</h2>
+                <h3>233 hotels</h3>
+
+             </div>
+        </div>
+        <div className="propertyListItem">
+            <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className='propertyListImg'
+             alt="" />
+             <div className="propertListTitle">
+                <h2>hotel</h2>
+                <h3>233 hotels</h3>
+
+             </div>
+        </div>
+        <div className="propertyListItem">
+            <img src="https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className='propertyListImg'
+             alt="" />
+             <div className="propertListTitle">
+                <h2>hotel</h2>
+                <h3>233 hotels</h3>
+
+             </div>
+        </div>
+        <div className="propertyListItem">
+            <img src="https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className='propertyListImg'
+             alt="" />
+             <div className="propertListTitle">
+                <h2>Hotel</h2>
+                <h3>233 hotels</h3>
+
+             </div>
+        </div>
+        <div className="propertyListItem">
+            <img src="https://images.pexels.com/photos/1438832/pexels-photo-1438832.jpeg?auto=compress&cs=tinysrgb&w=600"
+            className='propertyListImg'
+             alt="" />
+             <div className="propertListTitle">
+                <h2>Hotel</h2>
+                <h3 className='H2HEADING'>233 hotels</h3>
+
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default PropertyList
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css"
 import Navbar from '../../components/Navbar/Navbar'
 import Header from '../../components/Header/Header'
 import Featured from '../../components/Featured/Featured'
+import PropertyList from '../../components/propertyList/PropertyList'
 
 
 
@@ -13,7 +14,8 @@ function Home() {
         <Header/>
         <div className="homeContainer">
           <Featured/>
-          
+          <h1 className='homeTitle'>Browse by property type</h1>
+          <PropertyList/>
         </div>
     </div>
   )
